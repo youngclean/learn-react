2e16661a718ce23477a4e7e728a5ad7ac429bb2d
unknown <fengyang.yj@alibaba-inc.com>
2016-07-11 16:15:44 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # learn-react
 react入门学习
+
+启动本地服务器，我用的是express框架，简单，上手快
+配置get,post请求见app.js
+
---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var COMMENTS_FILE = path.join(__dirname, 'learn-react/examples/comments/data/comments.json');
+//console.log(COMMENTS_FILE);
+app.get('/api/comments', function(req, res) {
+    // console.log(req.body);
+    fs.readFile(COMMENTS_FILE, function(err, data) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        res.json(JSON.parse(data));
+    });
+});
+
+app.post('/api/comments', function(req, res) {
+    // console.log(req.body);
+    fs.readFile(COMMENTS_FILE, function(err, data) {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+        var comments = JSON.parse(data);
+        // NOTE: In a real implementation, we would likely rely on a database or
+        // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+        // treat Date.now() as unique-enough for our purposes.
+        var newComment = {
+            id: Date.now(),
+            author: req.body.author,
+            text: req.body.text,
+        };
+        comments.push(newComment);
+        fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            res.json(comments);
+        });
+    });
+});
+
+module.exports = app;
