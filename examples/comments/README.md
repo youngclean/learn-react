775a4decf4320321d6b406971ef1e47647e640cb
unknown <fengyang.yj@alibaba-inc.com>
2016-07-15 18:17:58 +0800

    更新read.me,增加example

---
+++ examples/comments/README.md
@@ -0,0 +1,47 @@
+#  构建一个具备评论功能的评论列表
+
+
+提供了
+
+1、评论列表
+
+2、提交评论表单
+
+3、后端请求的钩子
+
+灵活的特性
+
+1、提交评论后，快速的显示在界面
+
+2、在线更新评论
+
+3、使用Markdown格式化文本
+
+组件结构
+
+- commentBox
+  - commentList
+      - comment
+  - CommentForm
+
+出自react教材 https://devdocs.io/react/docs/tutorial
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- examples/comments/scripts/example.js
+++ examples/comments/scripts/example.js
@@ -1,19 +1,3 @@
-/**
- * 出自react教材 https://devdocs.io/react/docs/tutorial
- * 提供了
- * 1、评论列表
- * 2、提交评论表单
- * 3、后端请求的钩子
- * 灵活的特性
- * 1、提交评论后，快速的显示在界面
- * 2、在线更新评论
- * 3、使用Markdown格式化文本
- * 组件结构
- * commentBox
- *   - commentList
- *       - comment
- *   - CommentForm
- */
 var Comment = React.createClass({
     rawMarkup: function() {
         var rawMarkup = marked(this.props.children.toString(), {
